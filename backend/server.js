69137d85b906450fed0860b5a1e45e5aa007edda
Bastien Durand <bastiendurand@Bastiens-MacBook-Pro.local>
2023-08-01 09:37:54 +0200

    changed server port to 8080 to avoid local conflict + installed react for frontend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { main } from "./db.js";
 import { createUser } from "./controller/userController.js";
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.json());
 
